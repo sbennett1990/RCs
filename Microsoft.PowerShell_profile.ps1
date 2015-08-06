35be1a07ce4ec0cfb70b07b6ba2c1ede5be02a52
Scott Bennett <sbennett1990@users.noreply.github.com>
2015-08-06 11:02:19 -0400

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,10 @@
+Remove-Item alias:\cat
+
+new-item alias:c -value Clear-Host
+new-item alias:l -value ls
+new-item alias:cat -value C:\Users\sbennet\bin\cat.exe
+new-item alias:fcat -value C:\Users\sbennet\bin\fcat.exe
+new-item alias:grep -value Select-String
+new-item alias:java -value C:\IBM\WebSphere\AppServer\java\bin\java.exe
+new-item alias:javac -value C:\IBM\WebSphere\AppServer\java\bin\javac.exe
+new-item alias:commands -value Get-Command
